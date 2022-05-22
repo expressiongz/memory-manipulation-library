cd05146f50c623e185f73f0f812312528f4d2a02
expressxen <97218884+expressionLux@users.noreply.github.com>
2022-05-22 16:37:49 +0100

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,11 +2,7 @@
 #include "includes.h"
 #include <thread>
 void main_thread(HMODULE mod_handle, const std::string& dll_name) {
-	auto addr = 0xC73EF;
-	std::array<std::uint8_t, 2> bytes = {0xFF, 0x00};
 	memorypatch_lib memlib(mod_handle, dll_name);
-	memlib.deflat(addr);
-	memlib.mem_set_bytes(2, bytes);
 	memlib.unload();
 }
 
